e6e70f5275282a7d977a7bfdd49f457b5abbd70f
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-12-27 14:13:51 +0530

    Added styled components

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: palevioletred;
+  color: white;
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
 
 ReactDOM.render(
-  <div>Popup</div>,
+  <Button>Popup</Button>,
   document.getElementById('popup')
 )
